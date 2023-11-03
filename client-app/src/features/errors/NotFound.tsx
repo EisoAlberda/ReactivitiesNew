4335e8c131ed6b68e5d0f01b57dd292e4a2d728a
Eiso Alberda <eisoalberda@gmail.com>
2023-11-03 10:49:51 +0100

    End of Section 10

---
+++ client-app/src/features/errors/NotFound.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { Button, Header, Icon, Segment } from "semantic-ui-react";
+
+export default function NotFound() {
+    return (
+        <Segment placeholder>
+            <Header icon>
+                <Icon name='search' />
+                Page not found
+
+            </Header>
+            <Segment.Inline>
+                <Button as={Link} to='/activities'>
+                    Return to Activities Page
+                </Button>
+            </Segment.Inline>
+        </Segment>
+    )
+}
