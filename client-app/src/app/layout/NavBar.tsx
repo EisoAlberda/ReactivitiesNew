a80799447b16a84c8b8e373dd89b1e40d23a78ff
Eiso Alberda <eisoalberda@gmail.com>
2023-11-13 14:13:06 +0100

    End of Section 17

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -9,7 +9,7 @@ export default observer(function NavBar() {
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} to='/' header>
-                    <img src="assets/logo.png" alt='logo' style={{marginRight: '10px'}}/>
+                    <img src="/assets/logo.png" alt='logo' style={{marginRight: '10px'}}/>
                     Reactivities
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities'/>
@@ -21,7 +21,7 @@ export default observer(function NavBar() {
                     <Image src={user?.image || '/assets/user.png'} avatar spaced="right" />
                     <Dropdown pointing='top left' text={user?.displayName}>
                         <Dropdown.Menu>
-                            <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text="My Profile" icon="user" />
+                            <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text="My Profile" icon="user" />
                             <Dropdown.Item onClick={logout} text='Logout' icon='power' /> 
                         </Dropdown.Menu>
                     </Dropdown>
