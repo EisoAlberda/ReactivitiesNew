c61bda6ad38182ebc84229d7bb7911a7ac487ab7
Eiso Alberda <eisoalberda@gmail.com>
2023-11-14 11:57:11 +0100

    End of Section 18

--- client-app/src/api/agent.ts
+++ client-app/src/api/agent.ts
@@ -6,100 +6,113 @@ import { store } from "../app/stores/store";
 import { User, UserFormValues } from "../app/models/user";
 import { Photo, Profile } from "../app/models/profile";
 
-const sleep = (delay:number) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, delay)
-    })
-}
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = "http://localhost:5000/api";
 
-const responseBody = <T> (response : AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-axios.interceptors.request.use(config => {
-    const token = store.commonStore.token;
-    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-})
+axios.interceptors.request.use((config) => {
+  const token = store.commonStore.token;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-axios.interceptors.response.use(async response => {
-        await sleep(1000);
-        return response;
-}, (error: AxiosError) => {
-    const {data, status, config} = error.response as AxiosResponse;
+axios.interceptors.response.use(
+  async (response) => {
+    await sleep(1000);
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
-        case 400:
-            if(config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')) {
-                router.navigate('/not-found');
+      case 400:
+        if (
+          config.method === "get" &&
+          Object.prototype.hasOwnProperty.call(data.errors, "id")
+        ) {
+          router.navigate("/not-found");
+        }
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
             }
-            if(data.errors) {
-                const modalStateErrors = [];
-                for (const key in data.errors) {
-                    if (data.errors[key]) {
-                        modalStateErrors.push(data.errors[key])
-                    }
-                }
-                throw modalStateErrors.flat();
-            } else {
-                toast.error(data);
-            }
-            break;
-        case 401:
-            toast.error('unauthorized')
-            break;
-        case 403:
-            toast.error('forbidden')
-            break;
-        case 404:
-            router.navigate('/not-found')
-            break;
-        case 500:
-            store.commonStore.setServerError(data);
-            router.navigate('/server-error');
-            break;
+          }
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("unauthorized");
+        break;
+      case 403:
+        toast.error("forbidden");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        store.commonStore.setServerError(data);
+        router.navigate("/server-error");
+        break;
     }
     return Promise.reject(error);
-})
+  }
+);
 
 const requests = {
-    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    del: <T> (url: string) => axios.delete(url).then<T>(responseBody),
-}
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete(url).then<T>(responseBody),
+};
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
-    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: ActivityFormValues) => requests.post<void>('activities', activity),
-    update: (activity: ActivityFormValues) => requests.put<void>(`activities/${activity.id}`, activity),
-    delete: (id: string) => requests.del<void>(`/activities/${id}`),
-    attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
-}
+  list: () => requests.get<Activity[]>("/activities"),
+  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+  create: (activity: ActivityFormValues) =>
+    requests.post<void>("activities", activity),
+  update: (activity: ActivityFormValues) =>
+    requests.put<void>(`activities/${activity.id}`, activity),
+  delete: (id: string) => requests.del<void>(`/activities/${id}`),
+  attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {}),
+};
 
 const Account = {
-    current: () => requests.get<User>('/account'),
-    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
-    register: (user: UserFormValues) => requests.post<User>('/account/register', user)
-}
+  current: () => requests.get<User>("/account"),
+  login: (user: UserFormValues) => requests.post<User>("/account/login", user),
+  register: (user: UserFormValues) =>
+    requests.post<User>("/account/register", user),
+};
+
 
 const Profiles = {
-    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
-    uploadPhoto: (file:Blob) => {
-        let formData = new FormData();
-        formData.append('File', file);
-        return axios.post<Photo>('photos', formData, {
-            headers: {'Content-Type': 'multipart/form-data'}
-        })
-    },
-    setMainPhoto: (id: string) => requests.post( `/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
-}
+  get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+  uploadPhoto: (file: Blob) => {
+    let formData = new FormData();
+    formData.append("File", file);
+    return axios.post<Photo>("photos", formData, {
+      headers: { "Content-type": "multipart/form-data" },
+    });
+  },
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+  updateProfile: (profile: Partial<Profile>) =>
+    requests.put(`/profiles`, profile),
+};
 
 const agent = {
-    Activities,
-    Account,
-    Profiles
-}
+  Activities,
+  Account,
+  Profiles,
+};
 
 export default agent;
